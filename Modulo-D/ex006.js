898ad5638733ba64dd431e7756b85036e283fe25
David Rodrigues <119936741+davidrodriguex@users.noreply.github.com>
2026-04-04 16:12:01 -0300

    Duas praticas simples de condicionais

---
+++ Modulo-D/ex006.js
@@ -0,0 +1,6 @@
+var vel = 61
+console.log(`Sua velocidade atual é de ${vel} km/h`)
+if (vel > 60){
+    console.log(`Você foi MULTADO!`)
+}
+console.log("Coloque o cinto de segurança e pratique direção defensiva.")
---
+++ Modulo-D/ex007.js
@@ -0,0 +1,7 @@
+var country = 'Franca'
+if (country == 'Brasil'){
+    console.log(`Você é de nacionalidade brasileira.`)
+}else{
+    console.log(`Seu pais é a ${country}.`)
+    console.log("Sua nacionalidade é estrangeira.")
+}
