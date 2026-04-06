57dc61408898e1440be1dcb97549dc77663043e1
David Rodrigues <119936741+davidrodriguex@users.noreply.github.com>
2026-04-06 10:58:49 -0300

    Finalizando atividade de acordo com meu entendimento

--- Modulo-D/praticas/ex013/script.js
+++ Modulo-D/praticas/ex013/script.js
@@ -9,16 +9,26 @@ var hora = date.getHours()
 texto.innerHTML = `Agora são ${hora} horas.`
 
 if(hora >= 5 && hora < 12){
+    cor.style.backgroundColor = '#ffe091'
     saudacao.innerHTML = 'Bom Dia!'
-    img.innerHTML = '<img src="img/nascer_do_sol.jpg" alt="Nascer do Sol" style ="border-radius: 50%; width: 70%; aspect-ratio: 1/1; object-fit: cover; overflow: hidden;"></img>'
+    img.innerHTML = '<img src="img/nascer_do_sol.jpg" alt="Nascer do Sol" class="foto"></img>'
 }else if(hora >= 12 && hora < 18){
+    cor.style.backgroundColor = '#fab428'
     saudacao.innerHTML = 'Boa Tarde!'
-    img.innerHTML = '<img src="img/por_do_sol.jpg" alt="Por do Sol" style ="border-radius: 50%; width: 70%; aspect-ratio: 1/1; object-fit: cover; overflow: hidden;"></img>'
+    img.innerHTML = '<img src="img/por_do_sol.jpg" alt="Por do Sol" class="foto"></img>'
 }else{
     if(hora >= 18){
+        cor.style.backgroundColor = '#2c3e50'
+        cor.style.color = '#fff'
+        texto.style.color = '#000'
+        saudacao.style.color = '#000'
         saudacao.innerHTML = 'Boa Noite!'
     }else{
+        cor.style.backgroundColor = '#0f2027'
+        cor.style.color = '#fff'
+        texto.style.color = '#000'
+        saudacao.style.color = '#000'
         saudacao.innerHTML = 'Boa Madrugada!'
     }
-    img.innerHTML = '<img src="img/boa_noite.jpg" alt="Noite Estrelada" style ="border-radius: 50%; width: 70%; aspect-ratio: 1/1; object-fit: cover; overflow: hidden;"></img>'
+    img.innerHTML = '<img src="img/boa_noite.jpg" alt="Noite Estrelada" class="foto"></img>'
 }
