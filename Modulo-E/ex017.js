39865489af7ebd5782a998b5443c9efefd3f75f4
David Rodrigues <119936741+davidrodriguex@users.noreply.github.com>
2026-04-08 15:14:34 -0300

    inciando prox ex

---
+++ Modulo-E/ex017.js
@@ -0,0 +1,16 @@
+var i = 0
+var f = 10
+var p = 2
+/*i = 10
+f = 0*/
+if( i<= f){
+    while(i <= f){
+    console.log(i)
+     i = i + p
+    }
+}else{
+    while(f <= i){
+        console.log(i)
+        i = i - p
+    }
+}
