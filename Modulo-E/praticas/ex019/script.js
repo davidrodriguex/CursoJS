4ebe069a59ed9c2a0378b30d8c3be9711492c2b1
David Rodrigues <119936741+davidrodriguex@users.noreply.github.com>
2026-04-13 13:28:26 -0300

    Falta a validação do botão de finalizar

--- Modulo-E/praticas/ex019/script.js
+++ Modulo-E/praticas/ex019/script.js
@@ -2,8 +2,7 @@ let num = document.getElementById('inum')
 let select_1 = document.getElementById('saida_1')
 let saida = document.getElementById('final-output')
 
-let add = document.getElementById('btn_1').addEventListener('click', adicionar)
-//let fin = document.getElementById('btn_2').addEventListener('click', finalizar)
+document.getElementById('btn_1').addEventListener('click', adicionar)
 
 let lista_num = []
 
@@ -17,4 +16,29 @@ function adicionar(){
         item.text = `O número ${valor} foi adicionado!`
         select_1.appendChild(item)
     }
+}
+
+document.getElementById('btn_2').addEventListener('click', finalizar)
+
+function finalizar(){
+    saida.innerHTML += '<strong>Resultado da Análise</strong><br><br>'
+    saida.innerHTML += `<strong>Total de números cadastrados:</strong> ${lista_num.length}<br>`
+    let maior = 0
+    let menor = 101
+    let soma = 0
+    let media
+    for(let cont in lista_num){
+        if(lista_num[cont] > maior){
+            maior = lista_num[cont]
+        }
+        if(lista_num[cont] < menor){
+            menor = lista_num[cont]
+        }
+        soma = soma + lista_num[cont]
+    }
+    media = soma / lista_num.length
+    saida.innerHTML += `<strong>Maior número adicionado:</strong> ${maior}<br>`
+    saida.innerHTML += `<strong>Menor número adicionado:</strong> ${menor}<br>`
+    saida.innerHTML += `<strong>Soma dos números adicionados:</strong> ${soma}<br>`
+    saida.innerHTML += `<strong>Media dos números adicionados:</strong> ${media}<br>`
 }
