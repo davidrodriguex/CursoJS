6cbecf5533623abc673c233f97a40222e1a047e6
David Rodrigues <119936741+davidrodriguex@users.noreply.github.com>
2026-04-13 15:47:05 -0300

    Finalizando o exercicio final

--- Modulo-E/praticas/ex019/script.js
+++ Modulo-E/praticas/ex019/script.js
@@ -10,21 +10,31 @@ function adicionar(){
     let valor = Number(num.value)
     if(!num.value || valor < 1 || valor > 100){
         alert('[ERRO] Digite um valor válido')
+    }else if(lista_num.includes(valor)){
+        alert('[ERRO] Valor já adicionado, por favor, escolha outro.')
     }else{
+        num.value = ''
+        num.focus()
         let item = document.createElement('option')
         lista_num.push(valor)
         item.text = `O número ${valor} foi adicionado!`
         select_1.appendChild(item)
+        saida.innerHTML = ''
     }
 }
 
 document.getElementById('btn_2').addEventListener('click', finalizar)
 
 function finalizar(){
+    if(lista_num.length === 0){
+        alert('[ERRO] Digite e adicione um valor.')
+        return
+    }
+    saida.innerHTML = ''
     saida.innerHTML += '<strong>Resultado da Análise</strong><br><br>'
     saida.innerHTML += `<strong>Total de números cadastrados:</strong> ${lista_num.length}<br>`
-    let maior = 0
-    let menor = 101
+    let maior = lista_num[0]
+    let menor = lista_num[0]
     let soma = 0
     let media
     for(let cont in lista_num){
