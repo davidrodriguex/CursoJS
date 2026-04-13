f1cbf0d5f72e6ecfb13cc0e72ce81a51237c119a
David Rodrigues <119936741+davidrodriguex@users.noreply.github.com>
2026-04-13 09:26:52 -0300

    Finalizando a saida no select

--- Modulo-E/praticas/ex019/script.js
+++ Modulo-E/praticas/ex019/script.js
@@ -1,20 +1,20 @@
-const { createElement } = require("react")
-
 let num = document.getElementById('inum')
 let select_1 = document.getElementById('saida_1')
 let saida = document.getElementById('final-output')
 
 let add = document.getElementById('btn_1').addEventListener('click', adicionar)
-let finalizar = document.getElementById('btn_2').addEventListener('click', finalizar)
+//let fin = document.getElementById('btn_2').addEventListener('click', finalizar)
 
 let lista_num = []
 
 function adicionar(){
     let valor = Number(num.value)
-    if((!num.value) || valor < 1 || valor > 100){
+    if(!num.value || valor < 1 || valor > 100){
         alert('[ERRO] Digite um valor válido')
     }else{
+        let item = document.createElement('option')
         lista_num.push(valor)
-        saida.value += ('Lista: ', lista_num)
+        item.text = `O número ${valor} foi adicionado!`
+        select_1.appendChild(item)
     }
 }
